63161a25f855bd1b4f0cb4a8cd90d072d09201e9
mbam <elledhons@gmail.com>
2022-02-17 20:04:08 +0700

    add list menu

--- src/components/ListCategori.js
+++ src/components/ListCategori.js
@@ -1,13 +1,72 @@
-import React, { Component } from 'react'
-import { Col } from 'react-bootstrap'
+import axios from "axios";
+import React, { Component } from "react";
+import { Col, ListGroup } from "react-bootstrap";
+import { API_URL } from "../utils/constants";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUtensils,
+  faCoffee,
+  faCheese,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Icon = ({ nama }) => {
+  if (nama === "Makanan")
+    return <FontAwesomeIcon icon={faUtensils} className="mr-2" />;
+  if (nama === "Minuman") return <FontAwesomeIcon icon={faCoffee} />;
+  if (nama === "Cemilan")
+    return <FontAwesomeIcon icon={faCheese} className="mr-2" />;
+  return <FontAwesomeIcon icon={faUtensils} className="mr-2" />;
+};
 
 export default class ListCategori extends Component {
+  //rconst
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(API_URL + "categories")
+      .then((res) => {
+        const categories = res.data;
+        this.setState({ categories });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
+    //console.log("categories : ", this.state.categories);
+    const { categories } = this.state;
+    const { changeCategory, categoriDipilih } = this.props;
     return (
-      <Col md={2} mt="2"> 
-      <h4><strong>Daftar Categori</strong></h4>
-      <hr />
+      <Col md={2} mt="2">
+        <h4>
+          <strong>Daftar Categori</strong>
+        </h4>
+        <hr />
+        <ListGroup>
+          {categories &&
+            categories.map((category) => (
+              <ListGroup.Item
+                key={category.id}
+                onClick={() => changeCategory(category.nama)}
+                className={
+                  categoriDipilih === category.nama && "category-aktif"
+                }
+              >
+                <h5>
+                  <Icon nama={category.nama} /> {category.nama}
+                </h5>
+              </ListGroup.Item>
+            ))}
+        </ListGroup>
       </Col>
-    )
+    );
   }
 }
